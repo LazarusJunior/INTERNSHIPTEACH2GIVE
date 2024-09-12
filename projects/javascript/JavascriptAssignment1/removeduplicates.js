810638d615a87d1cc30cbbaadfd9bc7199313dd2
LazarusJunior <keyajunior9@gmail.com>
2024-09-12 15:59:32 +0300

    javascript task 3

--- projects/javascript/JavascriptAssignment1/removeduplicates.js
+++ projects/javascript/JavascriptAssignment1/removeduplicates.js
@@ -1,7 +1,7 @@
 function removeDuplicates(str) {
     return [...new Set(str)].join('');
   }
-  
+   
 
   console.log(removeDuplicates('programming'));
   console.log(removeDuplicates('hello world'));
